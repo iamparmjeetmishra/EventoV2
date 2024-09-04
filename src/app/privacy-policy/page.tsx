4e36dc7093f62ef0977a60280ed0d3259954faf8
Parmjeet <iamparmjeetmishra@gmail.com>
2024-09-04 21:08:18 +0530

    17EventoV2->TnC and Privacy page added

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,6 +1,47 @@
+import H1 from "@/components/h1";
 
 export default function Page() {
-  return (
-    <div>page</div>
-  )
+	return (
+		<div className="p-6 md:py-10 max-w-[900px] mx-auto">
+			<H1>Dummy Privacy Policy</H1>
+			<p>
+				At Website Name, accessible at Website.com, one of our main
+				priorities is the privacy of our visitors. This Privacy Policy
+				document contains types of information that is collected and
+				recorded by Website Name and how we use it. If you have
+				additional questions or require more information about our
+				Privacy Policy, do not hesitate to contact us through email at
+				Email@Website.com This privacy policy applies only to our
+				online activities and is valid for visitors to our website
+				with regards to the information that they shared and/or
+				collect in Website Name. This policy is not applicable to any
+				information collected offline or via channels other than this
+				website.
+			</p>
+			<H1 className="lg:text-3xl text-xl">Consent</H1>
+			<p>
+				By using our website, you hereby consent to our Privacy Policy
+				and agree to its terms.
+			</p>
+			<H1 className="lg:text-3xl text-xl">Information we collect</H1>
+			<p>
+				The personal information that you are asked to provide, and
+				the reasons why you are asked to provide it, will be made
+				clear to you at the point we ask you to provide your personal
+				information. If you contact us directly, we may receive
+				additional information about you such as your name, email
+				address, phone number, the contents of the message and/or
+				attachments you may send us, and any other information you may
+				choose to provide. When you register for an Account, we may
+				ask for your contact information, including items such as
+				name, company name, address, email address, and telephone
+				number.
+			</p>
+
+			<H1 className="lg:text-3xl text-xl">Cookies and Web Beacons</H1>
+			<p>
+      Like any other website, Website Name uses cookies. These cookies are used to store information including {"visitors'"} preferences, and the pages on the website that the visitor accessed or visited. The information is used to optimize the {"users'"} experience by customizing our web page content based on {"visitors'"} browser type and/or other information.
+			</p>
+		</div>
+	);
 }
